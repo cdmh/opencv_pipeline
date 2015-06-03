b97225aeef7f2eb7401a544b526f4c8ea239aefb
Craig Henderson <hello@craighenderson.co.uk>
2015-06-03 10:00:51 +0100

    license plate recognition example

--- test/test_pipeline.cpp
+++ test/test_pipeline.cpp
@@ -76,9 +76,128 @@ void play_grey_video()
     cvDestroyAllWindows();
 }
 
+
+// http://rnd.azoft.com/instant-license-plate-recognition-in-ios-apps/
+cv::Mat preprocess_license_plate(cv::Mat const &src)
+{
+    using namespace cv_pipeline;
+    return src
+         | dilate(3, 9)                          // close & subtract
+         | erode(3, 9)
+         | subtract(src)
+         | cv::abs
+         | sobel(1, 0, 3)                        // edges & blur
+         | gaussian_blur(5, 5)
+         | dilate(3, 9)                          // closing
+         | erode(3, 9)
+         | convert(CV_8UC1)
+         | threshold(0., 255.);                  // create binary image
+}
+
+void license_plate()
+{
+    using namespace cv_pipeline;
+
+    char const * const filename = "../../../test data/images/vehicle-license-plate-recognition-algorithm-02.jpg";
+
+    cv::Mat src = filename | verify | gray;// | convert(CV_64FC1);
+    preprocess_license_plate(src);
+    cv::Mat mask = preprocess_license_plate(src);
+
+    cv::Mat overlay;
+    cv::merge(
+        { cv::Mat::zeros(mask.rows, mask.cols, CV_8UC1),
+          cv::Mat::zeros(mask.rows, mask.cols, CV_8UC1),
+          mask
+        },
+        overlay);
+
+    double alpha = 0.6;
+    cv::Mat dst;
+    addWeighted(src | color_space(cv::COLOR_GRAY2BGR), alpha, overlay, 1.-alpha, 0.0, dst);
+
+
+// step1
+    std::vector<std::vector<cv::Point>> contours;  
+    findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);  
+
+// step2
+    std::vector<cv::RotatedRect> rects;
+    for (auto itc = contours.cbegin(); itc != contours.cend(); )
+    {
+        cv::RotatedRect mr = minAreaRect(cv::Mat(*itc));
+
+        auto const area   = fabs(contourArea(*itc));
+        auto const bbArea = mr.size.width * mr.size.height;
+        auto const ratio  = area/bbArea;
+                
+        if (ratio < 0.45  ||  bbArea < 400)
+            itc = contours.erase(itc);
+        else
+        {
+            ++itc;
+            rects.push_back(mr);
+        }
+    }
+
+
+    auto result = src  | color_space(cv::COLOR_GRAY2BGR);
+    for (auto &rect : rects)
+    {
+        rect.size.width  += rect.size.width  * 0.15126f;
+        rect.size.height += rect.size.height * 0.625f;
+
+        // step 7
+        std::vector< std::vector<cv::Point> > contours;  
+        findContours(src(rect.boundingRect() & roi(src)) | threshold(0., 255.), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+        double largest = 0.;
+        size_t index = std::numeric_limits<size_t>::max();
+        for (size_t i=0; i<contours.size(); ++i)
+        {
+            auto area = contourArea(contours[i], false);
+            if (area > largest)
+            {
+                largest = area;
+                index = i;
+            }
+        }
+
+        if (index != std::numeric_limits<size_t>::max())
+        {
+            cv::RotatedRect box = minAreaRect(cv::Mat(contours[index]));
+
+            double angle = box.angle;
+            if (angle < -45.)
+                angle += 90.;
+
+            cv::Point2f vertices[4];
+            box.points(vertices);
+
+            for(int i = 0; i < 4; ++i)
+            {
+                line(
+                    result(rect.boundingRect() & roi(result)),
+                    vertices[i],
+                    vertices[(i + 1) % 4],
+                    cv::Scalar(255, 255, 0));
+            }
+        }
+    }
+}
+
 void exhaustive()
 {
     using namespace cv_pipeline;
+
+    try
+    {
+        license_plate();
+    }
+    catch (exceptions::file_not_found const &e)
+    {
+        std::cerr << "File not found: " << e.what() << '\n';
+    }
 
 #if 0
     // test webcam
