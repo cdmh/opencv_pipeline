118602eaa237cdab20d5f17b29feb2b7e347db75
Craig Henderson <hello@craighenderson.co.uk>
2016-11-25 10:24:24 +0000

    rationalised headers

--- test/stdafx.h
+++ test/stdafx.h
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <iostream>
+
 //opencv
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/features2d/features2d.hpp>
-#include <opencv2/opencv.hpp>               // calcOpticalFlowPyrLK
 #include <opencv2/nonfree/nonfree.hpp>
 #include <opencv2/nonfree/features2d.hpp>
